eb10dee47f8bb1e407144f6a1ac7788a1edfd2c1
Danulo <danulo1337@gmail.com>
2025-10-29 20:15:49 +0200

    feat: add models for Task and Tag

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.name
+
+
+
+class Task(models.Model):
+    content = models.TextField(blank=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField(blank=True, null=True)
+    done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return f"{self.content[:20]}{'...' if len(self.content) > 20 else ''}"
